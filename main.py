af9c51c2bc79eeb7ab4155b830e41991c3de2a4a
Andrey Chervonyi <freezmain.2004@gmail.com>
2026-01-21 11:18:03 +0200

    del

--- main.py
+++ main.py
@@ -1,63 +1,26 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "SVIT-Helper" # Назва
-    page.window.icon = "icons/logo.png" # Іконка
-    page.theme_mode = 'black' # Тема
-    page.padding = 0
-    page.spacing = 0
+    page.title = "SVIT-Helper"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    main_text = ft.Text("Головний екран")
+    input = ft.TextField(value = "0", text_align = ft.TextAlign.RIGHT, width = 50)
 
-    #---- Зміна робочого розділу ----
-    def change_tab(element):
-        index = element.control.selected_index
-        if index == 0:
-            main_text.value = "Головний екран"
-        elif index == 1:
-            main_text.value = "Розрахунок модулів"
-        elif index == 2:
-            main_text.value = "Конфігуратор проєкту"
-        elif index == 3:
-            main_text.value = "Процес підключення"
-        elif index == 4:
-            main_text.value = "Налаштування"
-        page.update()
+    def button_minus(e):
+        input.value = str(int(input.value) - 1)
+    
+    def button_plus(e):
+        input.value = str(int(input.value) + 1)
 
-    #---- Вигляд застосунку -----
     page.add(
-        ft.Column(
-            [
-                ft.Container(
-                    bgcolor = "#18181c",
-                    height = 100,
-                    width = float("inf"),
-                    content = ft.Text("Хедер", color = "white"),
-                ),
-
-                ft.Row(
-                    [
-                        ft.Container(
-                            bgcolor = "#17171a",
-                            height = float("inf"),
-                            width = 150,
-                            content = ft.Text("Бокове меню", color = "white"),
-                        ),
-
-                        ft.Container(
-                            bgcolor = "#373840",
-                            height = float("inf"),
-                            expand = True,
-                            content = main_text,
-                        ),
-                    ],
-                    expand = True,
-                    spacing = 0,
-                )
+        ft.Row(
+            alignment = ft.MainAxisAlignment.CENTER,
+            controls = [
+                ft.IconButton(ft.Icons.REMOVE, on_click = button_minus),
+                input,
+                ft.IconButton(ft.Icons.ADD, on_click = button_plus),
             ],
-            expand = True,
-            spacing = 0,
         )
     )
 
-ft.app(target = main) # Запуск застосунку
+ft.run(main)
